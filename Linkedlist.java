6c4d87bcbeb22630526fd9a1ee19bc94b50abf29
Shyam Maddhesiya <shyammaddhesiya2005@gmail.com>
2026-09-18 12:34:16 +0530

    First

---
+++ Linkedlist.java
@@ -0,0 +1,54 @@
+public class Linkedlist {
+    static Node head;
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    void insertStart(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+        } else {
+            newNode.next = head;
+            head = newNode;
+        }
+    }
+
+    void insertEnd(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    void display() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        Linkedlist list = new Linkedlist();
+        list.insertStart(30);
+        list.insertStart(20);
+        list.insertEnd(40);
+        list.display();
+    }
+}
